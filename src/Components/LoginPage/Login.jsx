34abc3d7fe2bdc09a3af430b8918c66b19c3f5cf
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-26 23:37:34 +0600

    added updated code

--- src/Components/LoginPage/Login.jsx
+++ src/Components/LoginPage/Login.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
 
+    const handleLogIn = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="flex flex-col items-center w-full max-w-md mx-auto space-y-6 px-4">
@@ -12,16 +20,19 @@ const Login = () => {
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
-                    </div>
+                    <form onSubmit={handleLogIn}>
+                        <div className="card-body space-y-4">
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input type="email" name="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" name="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-neutral mt-4">Login</button>
+                            </fieldset>
+                        </div>
+                    </form>
+                    <p className="ml-4 mb-4">New to this website ? please signup first <Link to='/signup'> <span className="text-xl font-semibold text-green-600">SignUp</span> </Link></p>
                 </div>
             </div>
         </div>
--- src/Components/SignUpPage/SignUp.jsx
+++ src/Components/SignUpPage/SignUp.jsx
@@ -1,6 +1,13 @@
 
 const SignUp = () => {
 
+    const handleSignUp = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="flex flex-col items-center w-full max-w-md mx-auto space-y-6 px-4">
@@ -11,18 +18,20 @@ const SignUp = () => {
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="label">Name</label>
-                            <input type="text" className="input" placeholder="Full Name" />
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Sign Up</button>
-                        </fieldset>
-                    </div>
+                    <form onSubmit={handleSignUp}>
+                        <div className="card-body space-y-4">
+                            <fieldset className="fieldset">
+                                <label className="label">Name</label>
+                                <input type="text" name="name" className="input" placeholder="Full Name" />
+                                <label className="label">Email</label>
+                                <input type="email" name="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" name="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-neutral mt-4">Sign Up</button>
+                            </fieldset>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
